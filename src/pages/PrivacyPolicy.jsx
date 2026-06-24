7fb6e35216a4e3658bbd8d95df29e45299c5b40e
Oyinlola Abdulmuiz <abdulmuizoyinlola94@gmail.com>
2026-06-24 16:39:08 +0100

    new commit

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -327,7 +327,7 @@ const PrivacyPolicy = () => {
                   <p>Email: Mudet@gmail.com</p>
                   <p>Phone: +234 813 521 345</p>
                   <p>Address: NO 1, Onimalu Palace Street, Adegbayi Area, Egbeda Local Government, Ibadan, Oyo State</p>
-                  <p>Website: https://sunmencesolutions.com.ng</p>
+                  <p>Website: https://mudetrealsolution.com</p>
                 </div>
               </div>
 
